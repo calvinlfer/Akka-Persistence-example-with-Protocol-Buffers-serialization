9b04e0bb8134c5bfe7d9165fbef1365df9771bac
Calvin Fernandes <calvin.l.fer@gmail.com>
2016-02-16 18:05:01 -0500

    Added a snapshot store to prevent akka from giving us warnings about not having a snapshot store Added a runner so we can run commands and watch our persistent actor work

--- src/main/scala/com/experiments/Main.scala
+++ src/main/scala/com/experiments/Main.scala
@@ -1,12 +1,23 @@
 package com.experiments
 
 import akka.actor.{Props, ActorSystem}
-import com.typesafe.config.ConfigFactory
+
+import scala.language.postfixOps
 
 object Main extends App {
-  val config = ConfigFactory.load()
-  val system = ActorSystem("calculator-actor-system", config)
+  val system = ActorSystem("calculator-actor-system")
   val persistentCalculatorActor = system.actorOf(Props[Calculator])
+
+  // Send messages
+  import models.Models._
+  persistentCalculatorActor ! PrintResult
+  persistentCalculatorActor ! Add(2)
+  persistentCalculatorActor ! Multiply(2)
+  persistentCalculatorActor ! PrintResult
+
+  // Wait for 5 seconds before terminating
+  Thread sleep 5000
+  system terminate
 }
 
 
